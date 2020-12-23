b014393ed0d8efc8dd60e397ad513bcb51dac86c
Frank Grieger <frank.grieger@icloud.com>
2020-12-23 13:19:39 +0100

    Initial Commit

---
+++ myObjectDetector/myObjectDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  myObjectDetectorApp.swift
+//  myObjectDetector
+//
+//  Created by Frank Grieger on 23.12.20.
+//
+
+import SwiftUI
+
+@main
+struct myObjectDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
