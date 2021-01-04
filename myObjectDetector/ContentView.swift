795da3caf6bd48ac34f80bd0b09129780e52e9ac
Frank Grieger <frank.grieger@icloud.com>
2021-01-04 18:17:20 +0100

    CameraPreview created

--- myObjectDetector/ContentView.swift
+++ myObjectDetector/ContentView.swift
@@ -6,16 +6,60 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State private var objectName: String = "Object name"
+    @State private var accuracy: String = "Accuracy 0%"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Image("Photos-new-icon")
+            Text(objectName)
+            Text(accuracy)
+        }
+        .padding()
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// create a camera preview view
+struct CameraPreview: UIViewRepresentable {
+    // create a UIView subclass
+    class VideoPreviewView: UIView {
+        //We wan't the view's core animation layer to be of type AVCaptureVideoPreviewLayer
+        //That is why we override the UIView's layer type
+        override class var layerClass: AnyClass {
+            // set it to AVCaptureVideoPreviewLayer
+            AVCaptureVideoPreviewLayer.self
+        }
+        // create a get-only property that returns the UIView's layer cast as AVCaptureVideoPreviewLayer
+        var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+            return layer as! AVCaptureVideoPreviewLayer
+        }
+    }
+    
+    // struct will have a capture session
+    let session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> VideoPreviewView {
+        let view = VideoPreviewView()
+        view.backgroundColor = .black
+        view.videoPreviewLayer.cornerRadius = 0
+        view.videoPreviewLayer.session = session
+        view.videoPreviewLayer.connection?.videoOrientation = .portrait
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: VideoPreviewView, context: Context) {
+        //some code
     }
 }
